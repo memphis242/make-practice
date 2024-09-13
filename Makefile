f4c3be44421d5f39feddfc63e731580c02ef23f1
Abdullah Almosalami <abdullah.almosalami.eng@gmail.com>
2024-09-13 14:20:17 -0500

    Cleared warnings, added some level of inputs validation, and removed disabled ubsan because I'm getting a linker error for a couple of functions for it...

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ CROSS	=
 CC		= $(CROSS)gcc
 COMPILER_WARNING_FLAGS = -Wall -Wextra -Wconversion -Wdouble-promotion -Wnull-dereference -Wwrite-strings -Wformat=2 -Wcast-align=strict -Wswitch-enum -Wpedantic
 # COMPILER_SANITIZERS = -fsanitize=undefined -fsanitize=bool -fsanitize=bounds-strict
+COMPILER_SANITIZERS = -fsanitize=bool -fsanitize=bounds-strict
 COMPILER_OPTIMIZATION_LEVEL = -Og -g3
 CFLAGS = $(COMPILER_WARNING_FLAGS) $(COMPILER_SANITIZERS) $(COMPILER_OPTIMIZATION_LEVEL)
 LDFLAGS = 
--- lin_pid_calculator.c
+++ lin_pid_calculator.c
@@ -7,21 +7,22 @@ int main( int argc, char * argv[] )
 {
    int num_of_converted_items;
    unsigned int message_id;
-   _Bool p0;
-   _Bool p1;
-   unsigned char protected_id;
 
    if ( argc == 2 )
    {
       // Get the first argument as a number
-      message_id &= 0x3F;  // mask out upper two bits to 0
       num_of_converted_items = sscanf( argv[1], "%X", &message_id );
+      message_id &= 0x3F;  // mask out upper two bits to 0
       // Compute PID from parity bit equations specified in LIN 2.1 spec
       if ( num_of_converted_items > 0 )
       {
+         _Bool p0;
+         _Bool p1;
+         unsigned char protected_id;
+
          p0 =    GET_BIT_VAL( message_id, 0 ) ^ GET_BIT_VAL( message_id, 1 ) ^ GET_BIT_VAL( message_id, 2 ) ^ GET_BIT_VAL( message_id, 4 );
          p1 = ( ~( GET_BIT_VAL( message_id, 1 ) ^ GET_BIT_VAL( message_id, 3 ) ^ GET_BIT_VAL( message_id, 4 ) ^ GET_BIT_VAL( message_id, 5 ) ) ) & 0x01;
-         protected_id = ( p1 << 7 ) | ( p0 << 6 ) | message_id;
+         protected_id = ( p1 << 7 ) | ( p0 << 6 ) | (unsigned char) message_id;
 
          printf( "Message ID: \t%X\nProtected ID: \t%X\n", message_id, protected_id );
       }
