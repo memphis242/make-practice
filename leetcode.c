7a7c47d141da121fc1d66d509ee7f156d044a448
Abdullah Almosalami <abdullah.almosalami.eng@gmail.com>
2025-03-22 20:34:25 -0500

    addTwoNumbers initial draft

--- leetcode.c
+++ leetcode.c
@@ -1,7 +1,22 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+
+// You are given two non-empty linked lists representing two non-negative integers.
+// The digits are stored in reverse order, and each of their nodes contains a single digit.
+// Add the two numbers and return the sum as a linked list.
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+// Constraints:
+// - The number of nodes in each linked list is in the range [1, 100].
+// - 0 <= Node.val <= 9
+// - It is guaranteed that the list represents a number that does not have leading zeros.
+
+struct ListNode
+{
+   unsigned char val;
+   struct ListNode *next;
+};
 
 long long int int_pow(int base, int exponent)
 {
@@ -15,103 +30,95 @@ long long int int_pow(int base, int exponent)
    return result;
 }
 
-bool isPalindrome(int x)
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
 {
-   bool retVal = false;
+   // Locals
+   // Persistent
+   static const unsigned char MAX_ITERATIONS = 100;
+   static struct ListNode DefaultReturn = { .val = 0, .next = NULL };
+   // Autos
+   unsigned int a;
+   unsigned int b;
+   unsigned long long int sum;
+   unsigned char i = 1;
+   unsigned char j = 1;
+   unsigned char max_digits;
+   struct ListNode * list_head;
+   struct ListNode * iterator;
 
-   int digit_stack[11];
-   int top = -1;
-   long long int temp = (long int)x;
-
-   if ( (x < 0) || ( ((x % 10) == 0) && (x != 0) ) )
+   // Extract digits
+   // First number
+   iterator = l1;
+   a = iterator->val;
+   while ( (i < MAX_ITERATIONS) && (iterator->next != NULL) )
    {
-      // Leave retVal to initialized value
+      i++;
+      iterator = iterator->next;
+
+      // Extract next digit and shift into place
+      a += iterator->val * int_pow(10, i);
    }
-   else if ( (x % 10) == x )
+
+   // Second number
+   iterator = l2;
+   b = iterator->val;
+   while ( (i < MAX_ITERATIONS) && (iterator->next != NULL) )
    {
-      retVal = true;
+      i++;
+      iterator = iterator->next;
+
+      // Extract next digit and shift into place
+      b += iterator->val * int_pow(10, i);
+   }
+
+   // Sum
+   sum = a + b;
+
+   // Construct linked-list
+   // Determine number of digits needed
+   if ( i > j )
+   {
+      max_digits = i + 2;
    }
    else
    {
-      do
-      {
-         top++;
-         digit_stack[top] = temp % 10;
-         temp /= 10;
-      } while ( temp > 0 );   // Keep going until we get the last digit
+      max_digits = j + 2;
+   }
+   // Start constructing
+   for ( unsigned char k = 0; k < max_digits; k++ )
+   {
+      unsigned char digit;
+      struct ListNode * node;
 
-      temp = 0;
-      for ( int i = 0; i <= top; i++ )
+      digit = sum % 10;
+      sum /= 10;
+
+      node = (struct ListNode *)malloc(sizeof(struct ListNode));
+      if ( node == NULL )
       {
-         temp += ( (long long int)digit_stack[i] ) * int_pow(10, top - i);
+         return &DefaultReturn;
       }
+      if ( k == 0 )
+      {
+         list_head = node;
+      }
+      node->val = digit;
+      node->next = NULL;
+      iterator->next = node;
 
-      retVal = (temp == (long long int)x);
+      if ( sum == 0 )
+      {
+         break;
+      }
    }
 
-   return retVal;
+   // Prune leading zero if applicable
+   // Ehhh, shouldn't have to if we're breaking at (sum == 0)
+
+   return list_head;
 }
 
 int main(void)
 {
-   int test_num = 1234567899; // Example of a non-palindrome
-   if (isPalindrome(test_num))
-   {
-      printf("%d is a palindrome.\n", test_num);
-   }
-   else
-   {
-      printf("%d is not a palindrome.\n", test_num);
-   }
-
-   test_num = 11; // Example of a palindrome
-   if (isPalindrome(test_num))
-   {
-      printf("%d is a palindrome.\n", test_num);
-   }
-   else
-   {
-      printf("%d is not a palindrome.\n", test_num);
-   }
-
-   test_num = 121; // Example of a palindrome
-   if (isPalindrome(test_num))
-   {
-      printf("%d is a palindrome.\n", test_num);
-   }
-   else
-   {
-      printf("%d is not a palindrome.\n", test_num);
-   }
-   test_num = -121; // Example of a non-palindrome
-   if (isPalindrome(test_num))
-   {
-      printf("%d is a palindrome.\n", test_num);
-   }
-   else
-   {
-      printf("%d is not a palindrome.\n", test_num);
-   }
-
-   test_num = 10; // Example of a non-palindrome
-   if (isPalindrome(test_num))
-   {
-      printf("%d is a palindrome.\n", test_num);
-   }
-   else
-   {
-      printf("%d is not a palindrome.\n", test_num);
-   }
-
-   test_num = 0; // Example of a palindrome
-   if (isPalindrome(test_num))
-   {
-      printf("%d is a palindrome.\n", test_num);
-   }
-   else
-   {
-      printf("%d is not a palindrome.\n", test_num);
-   }
-
    return 0;
 }
